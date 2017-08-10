5b63d26b0e830691eed9f10828a7d88211965887
Brian Steele <zipptang@gmail.com>
2017-08-09 18:59:25 -0700

    Added express-handlebars and added views folder with basic layout.

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 'use strict'
 
 const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+
 const app = express()
+app.set('views', path.join(__dirname, 'views'))
+app.engine('handlebars', exphbs({
+    defaultLayout: 'layout'
+}))
+app.set('view engine', 'handlebars')
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index')
 })
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000.')
+// Set Port
+app.set('port', (process.env.PORT || 3000 ))
+
+app.listen(app.get('port'), () => {
+    console.log('Server started on port ' + app.get('port'))
 })
